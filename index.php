e311c6689d4c72e00f1e173897eb68efe8451c15
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-09-17 16:18:35 -0400

    Conflict fix?

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php include 'inc/top-nav.inc'; ?>
 
 <<<<<<< HEAD
-   <div class="container"> <h1><a href="lihui.php"> Lihui Guo</a></h1>
-=======
+   <div class="container"> 
     <h1>About Edmonton</h1>
 
     <h1><a href="lihui.php"> Lihui Guo</a></h1>
->>>>>>> a0e1d149e5af43e29a7e88f79edbe2928d4ab7b7
+
     <img src="images/lihui_photo.png" >
 
     <h1> <a href="aliye.php">Aliye Gallagher</a> </h1>
-    <img class="image-circle" src="images/aliye_photo.jpg">
+    <img  src="images/aliye_photo.jpg">
 
     <h1><a href="cassidy.php">Cassidy Clayton</a> </h1>
-    <img class = "img-circle" src="images/cassidy_photo.jpg">
+    <img  src="images/cassidy_photo.jpg">
     </div>
   </body>
 
