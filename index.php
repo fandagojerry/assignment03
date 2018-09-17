df2f210023010b5964538fd3d59fabd5fe35147b
Lihui Guo <lguo16@u.rochester.edu>
2018-09-17 15:57:53 -0400

    php includes

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'inc/top-nav.inc'; ?>
+
+    <h1>Lihui Guo</h1>
+    <img class="img-circle" src="images/lihui_photo.png" >
+
+    <h1>Aliye Gallagher</h1>
+    <img class="image-circle" src="images/aliye_photo.jpg">
+
+    <h1>Cassidy Clayton</h1>
+    <img class = "img-circle" src="images/cassidy_photo.jpg">
+  </body>
+
+  </html>
