b5366fadc1f96fea4e106b44808b2169a8b960e0
cassidyjclayton <cassidyjclayton@users.noreply.github.com>
2018-09-17 16:16:48 -0400

    path? container add
    trying to get a style in place, still trying to figure out why the css won't link

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include 'inc/top-nav.inc'; ?>
 
-    <h1><a href="lihui.php"> Lihui Guo</a></h1>
+   <div class="container"> <h1><a href="lihui.php"> Lihui Guo</a></h1>
     <img src="images/lihui_photo.png" >
 
     <h1> <a href="aliye.php">Aliye Gallagher</a> </h1>
@@ -8,6 +8,7 @@
 
     <h1><a href="cassidy.php">Cassidy Clayton</a> </h1>
     <img class = "img-circle" src="images/cassidy_photo.jpg">
+    </div>
   </body>
 
   </html>
