af30308fadb06500a9fe44e251f6ba8702871fb5
Aliye Gallagher <aliyegallagher@gmail.com>
2018-09-17 16:20:46 -0400

    index fixed for good I hope

--- index.php
+++ index.php
@@ -2,18 +2,13 @@
 
 <?php include 'inc/top-nav.inc'; ?>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-   <div class="container"> 
+   <div class="container">
     <h1>About Edmonton</h1>
 
     <h1><a href="lihui.php"> Lihui Guo</a></h1>
-
-=======
    <div class="container"> <h1><a href="lihui.php"> Lihui Guo</a></h1>
 
     <h1><a href="lihui.php"> Lihui Guo</a></h1>
->>>>>>> 31cbeab416492952a3081c188dbac9bb995d1d5c
     <img src="images/lihui_photo.png" >
 
     <h1> <a href="aliye.php">Aliye Gallagher</a> </h1>
