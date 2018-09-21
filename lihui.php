5b73bef58a0fcf40b44c1fc528c8833bd943984b
Aliye Gallagher <aliyegallagher@gmail.com>
2018-09-21 19:47:07 -0400

    lihui headers added

--- lihui.php
+++ lihui.php
@@ -5,17 +5,16 @@
   <div class="container">
     <h1>Lihui Guo</h1>
 
-  <h2>Early Life</h2>
+  <h2>Intro</h2>
   <p> I grew up in Brooklyn, New York in a predominantly Asian-American and Italian-American neighborhood. My parents had just moved from Japan/China to be with other family in New York City. I spent a lot of time with relatives as a child, particularly with my older cousins and grandparents. Our neighborhood had everything we needed – everything was within walking distance, and it was really nice to have family so close. My family was and still is super important to me. They’re always my biggest supporters and I really appreciate how my parents aren’t stereotypical “Asian tiger parents”.
   </p>
 
   <h2>Early Education</h2>
   <p>When I started elementary school, I didn’t speak much English, so I had to take ESL classes. I picked it up quickly, and started to love reading, which probably helped. I ended up testing into a magnet middle school for music, so I spent three years pretending I knew how to play the cello. The middle school had an advanced curriculum, which prepared me well for the SHSAT, a standardized exam New Yorkers take to gain admission into selective high schools. I scored pretty high, and went to the “#1 public high school in NYC”. It was super competitive, but they offered a wide variety of courses, like Organic Chemistry.
-    </p>
+  </p>
 
   <h2>College</h2>
-  <p>
-    Taking Orgo in high school convinced me I wanted to work with Chemistry for the rest of my life, so I applied to way too many colleges as a Chemical Engineering major. Fast forward eight months, and I wound up at the University of Rochester. I took my introductory engineering courses and realized I did not want to be in a lab for the rest of my life. I did take a super interested Economics course, and changed my major to Business by the end of my freshman year. It was one of the better decisions I’ve made, but I still missed using the more computational part of my brain, so I decided to give Computer Science another shot (I had taken a few classes in high school), and turned that into a minor.
+  <p>Taking Orgo in high school convinced me I wanted to work with Chemistry for the rest of my life, so I applied to way too many colleges as a Chemical Engineering major. Fast forward eight months, and I wound up at the University of Rochester. I took my introductory engineering courses and realized I did not want to be in a lab for the rest of my life. I did take a super interested Economics course, and changed my major to Business by the end of my freshman year. It was one of the better decisions I’ve made, but I still missed using the more computational part of my brain, so I decided to give Computer Science another shot (I had taken a few classes in high school), and turned that into a minor.
   </p>
 
   <h2>Work</h2>
