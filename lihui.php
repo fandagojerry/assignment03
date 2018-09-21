1431ac3f0f1cef1ae4e70915eebac8b61029313d
Aliye Gallagher <aliyegallagher@gmail.com>
2018-09-21 19:38:12 -0400

    lihui links updated

--- lihui.php
+++ lihui.php
@@ -26,7 +26,7 @@
 <footer>
   <p>Content from:</p>
   <ul>
-      <li><a href="http://csc174.org/assignment01/lguo/lab01/">Lihui Lab 1 Index</a></li>
+      <li><a href="http://csc174.org/assignment01/lguo/lab01/index.html">Lihui Lab 1 Index</a></li>
       <li><a href="http://csc174.org/assignment01/lguo/lab01/early-edu.html">Lihui Lab 1 Early Education</a></li>
       <li><a href="http://csc174.org/assignment01/lguo/lab01/college.html">Lihui Lab 1 College</a></li>
       <li><a href="http://csc174.org/assignment01/lguo/lab01/work-exp.html">Lihui Lab 1 Work</a></li>
