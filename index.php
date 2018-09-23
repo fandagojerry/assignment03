4e8a2a562a2396bda10407a4198a0d54e2740b74
Lihui Guo <lguo16@u.rochester.edu>
2018-09-23 16:18:18 -0400

    validate index

--- index.php
+++ index.php
@@ -2,25 +2,25 @@
 
 <div class = "container">
 
-  <h1><a href="lihui.php"> Lihui Guo</a></h1>
-  <img src="images/lihui_photo.png" class="profile" >
+  <h1><a href="lihui.php">Lihui Guo</a></h1>
+  <img src="images/lihui_photo.png" alt="Lihui" class="profile" >
 
   <h1> <a href="aliye.php">Aliye Gallagher</a> </h1>
-  <img src="images/aliye_photo.jpg" class="profile">
+  <img src="images/aliye_photo.jpg" alt="Aliye" class="profile">
 
   <h1><a href="cassidy.php">Cassidy Clayton</a> </h1>
-  <img src="images/cassidy_photo.jpg" class="profile">
+  <img src="images/cassidy_photo.jpg" alt="Cassidy" class="profile">
 
 </div>
+
+  <footer>
+    <a href=http://csc174.org/assignment01/lguo/lab01/>
+    <a href=http://csc174.org/assignment01/agallagher/Assignment%201/index.html>
+    <a href=http://csc174.org/assignment01/cclayton/lab-01/>
+  </footer>
 
   <?php include 'inc/menu-highlighter.inc'; ?>
 
 </body>
-
-<footer>
-  <a href=http://csc174.org/assignment01/lguo/lab01/>
-  <a href=http://csc174.org/assignment01/agallagher/Assignment%201/index.html>
-  <a href=http://csc174.org/assignment01/cclayton/lab-01/>
-</footer>
 
 </html>
